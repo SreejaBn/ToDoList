559e5f85bdf88f62640dc528b01ac1ef886872e5
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2025-06-17 20:38:57 +0530

    Add files via upload

---
+++ ToDoList.py
@@ -0,0 +1,41 @@
+todolist= []
+
+def AddTask():
+    task = input ('Enter your task: ')
+    todolist.append({'task': task, 'status': 'pending'})
+
+def DelTask():
+
+def ShowTask():
+    if len(todolist) == 0:
+        print ("The list is empty.")
+    else:
+        for index, task in enumerate(todolist, 1):
+            print (f'{index}. {task['task']} - {task['status']}\n')
+
+def CompTask():
+    
+if __name__ == '__main__':
+
+    print ("This is a ToDoList program.")
+    print ("Enter your choice in numbers.") 
+
+    choice = input("""1. Add task \n2. Delete task. \n3. Show Tasks \n4. Mark a task as complete \n5. Quit""")
+
+    if choice == 1:
+        AddTask()
+
+    elif choice == 2:
+        DelTask()
+
+    elif choice == 3:
+        ShowTask()
+
+    elif choice == 4:
+        CompTask()
+
+    elif choice == 5:
+        exit()
+
+    else:
+        print ("Enter a valid response.")
